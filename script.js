f5d0cda28b77ecd6d87428f437325bcd1c634ed9
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-08-23 21:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+
+	String email = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/.";
+
+	return email.test(str);
 }
 
 // Do not change the code below.
